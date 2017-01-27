ce863d8d06b78ad926670f4513d0a29714f2e8f2
raphael <raphaelts@gmail.com>
2017-01-27 18:36:13 -0200

    Added polytropic exponent and polytropic head.

--- prf/curves.py
+++ prf/curves.py
@@ -1,3 +1,4 @@
+import numpy as np
 from prf.state import *
 
 
@@ -74,9 +75,78 @@ class Curves:
         Examples
         --------
         """
+        # suction state
+        suc = State('HEOS', ps, Ts, fluid, **kwargs)
+
         # calculate head and efficiency for each point
-
-
 
     # TODO add **kwargs for units
     # TODO add constructor -> from_discharge_conditions
+
+
+def n_exp(suc, disch):
+    """Polytropic exponent.
+
+    Calculates the polytropic exponent given a suction and a discharge state.
+
+    Parameters:
+    -----------
+    suc : State
+        Suction state.
+    disch : State
+        Discharge state.
+
+    Returns:
+    --------
+    n_exp : float
+        Polytropic exponent.
+
+    Examples:
+    ---------
+
+    """
+    ps = suc.p()
+    vs = 1 / suc.rhomass()
+    pd = disch.p()
+    vd = 1 / disch.rhomass()
+
+    return np.log(pd/ps)/np.log(vs/vd)
+
+
+def head_pol(suc, disch):
+    """Polytropic head.
+
+    Calculates the polytropic head given a suction and a discharge state.
+
+    Parameters:
+    -----------
+    suc : State
+        Suction state.
+    disch : State
+        Discharge state.
+
+    Returns:
+    --------
+    n_exp : float
+        Polytropic head.
+
+    Examples:
+    ---------
+
+    """
+    n = n_exp(suc, disch)
+
+    p2 = disch.p()
+    v2 = 1 / disch.rhomass()
+    p1 = suc.p()
+    v1 = 1 / suc.rhomass()
+
+    return (n/(n-1))*(p2*v2 - p1*v1)
+
+# TODO add tests for n_exp
+# TODO add tests for head_pol
+# TODO add ef_pol
+# TODO add head_isen
+# TODO add ef_isen
+# TODO add schultz_factor
+# TODO add head_pol_schultz
