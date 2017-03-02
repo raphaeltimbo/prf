1d9c90a084b230f8a64354dd4fde8b2e149ca8e3
raphael <raphaelts@gmail.com>
2017-03-02 13:38:29 -0300

    Added plot of the discharge pressure.

--- prf/results.py
+++ prf/results.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-__all__ = ['plot_head_curve']
+__all__ = ['plot_head_curve', 'plot_disch_p_curve']
 
 sns.set_style('white', rc={'axes.grid':True,
                            'axes.linewidth': 0.1,
@@ -34,3 +34,25 @@ def plot_head_curve(imp, flow='flow_v', plot_current_point=True, ax=None):
     ax.set_ylabel('Head $(J / kg)$')
 
     return ax
+
+
+def plot_disch_p_curve(imp, flow='flow_v', plot_current_point=True, ax=None):
+    if ax is None:
+        ax = plt.gca()
+
+    flow_ = [getattr(p, flow) for p in imp.new_points]
+
+    flow = np.linspace(min(flow_), max(flow_), 100)
+
+    ax.plot(flow, imp.disch_p_curve(flow))
+    if plot_current_point is True:
+        ax.plot(imp.current_point.flow_v, imp.current_point.disch.p(), 'o')
+
+    if flow is 'flow_m':
+        ax.set_xlabel('Mass flow $(kg / s)')
+    else:
+        ax.set_xlabel('Volumetric flow $(m^3 / s)$')
+
+    ax.set_ylabel('Discharge Pressure $(Pa)$')
+
+    ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
