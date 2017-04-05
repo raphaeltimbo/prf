dd54ba08e1d68760a2ba95c9d97b23dac6247653
raphael <raphaelts@gmail.com>
2017-04-05 18:44:19 -0300

    Plot phase envelope.

--- prf/results.py
+++ prf/results.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-__all__ = ['plot_head_curve', 'plot_disch_p_curve', 'plot_eff_curve']
+__all__ = ['plot_head_curve', 'plot_disch_p_curve', 'plot_eff_curve',
+           'plot_envelope']
 
 sns.set_style('white', rc={'axes.grid':True,
                            'axes.linewidth': 0.1,
@@ -76,3 +77,34 @@ def plot_eff_curve(imp, flow='flow_v', plot_current_point=True, ax=None):
         ax.set_xlabel('Volumetric flow $(m^3 / s)$')
 
     ax.set_ylabel('Efficiency')
+
+
+def plot_envelope(state, ax=None):
+    """Plot phase envelop for a given state.
+    
+    Parameters
+    ----------
+    state : prf.State
+    
+    ax : matplotlib.axes, optional
+        Matplotlib axes, if None creates a new.
+        
+    Returns
+    -------
+    ax : matplotlib.axes
+        Matplotlib axes with plot.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    state.build_phase_envelope('')
+    p_e = state.get_phase_envelope_data()
+
+    ax.plot(p_e.T, p_e.p, '-')
+
+    ax.set_xlabel('Temperature $(K)$')
+    ax.set_ylabel('Pressure $(Pa)$')
+    ax.set_yscale('log')
+
+    return ax
+
