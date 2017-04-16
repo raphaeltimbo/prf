2a83258c40dae5d939a60847a633857c4e7b3a87
raphaeltimbo <raphaelts@gmail.com>
2017-04-15 22:22:39 -0300

    Changed cprofile.

--- prf/cprofile/cprofile.py
+++ prf/cprofile/cprofile.py
@@ -55,3 +55,46 @@ cProfile.run(
     'prf.Impeller(point, b=0.285, D=0.365)',
     'profile-Impeller-'+label
 )
+
+fluid = {'Methane': 0.69945,
+         'Ethane': 0.09729,
+         'Propane': 0.05570,
+         'n-Butane': 0.01780,
+         'Isobutane': 0.01020,
+         'n-Pentane': 0.00390,
+         'Isopentane': 0.00360,
+         'n-Hexane': 0.00180,
+         'Nitrogen': 0.01490,
+         'HydrogenSulfide': 0.00017,
+         'CarbonDioxide': 0.09259,
+         'Water': 0.00200}
+units = {'p_units': 'bar',
+         'T_units': 'degC',
+         'flow_m_units': 'kg/h',
+         'speed_units': 'RPM'}
+
+suc, head, eff = (prf.State.define(p=16.99, T=38.4, fluid=fluid, **units),  # suc
+                  140349.53763396584,  # head
+                  0.71121369651557265)                                  # eff
+
+
+p0 = prf.Point(suc=suc, head=head, eff=eff, flow_m=175171, speed=12204, **units)
+
+cProfile.run(
+    'prf.Point(suc=suc, head=head, eff=eff, flow_m=175171, speed=12204, **units)',
+    'profile-Point-from_such_head_eff'+label
+)
+
+p1 = prf.Point(suc=p0.suc, eff=p0.eff, volume_ratio=p0.volume_ratio,
+               speed=p0.speed, flow_m=p0.flow_m)
+
+cProfile.run(
+    'prf.Point(suc=p0.suc, eff=p0.eff, volume_ratio=p0.volume_ratio, speed=p0.speed, flow_m=p0.flow_m)',
+    'profile-Point-from_such_eff_vol'+label
+)
+
+cProfile.run(
+    'prf.Point(suc=point.suc, eff=point.eff, volume_ratio=point.volume_ratio, speed=point.speed, flow_m=point.flow_m)',
+    'profile-Point_exp-from_such_eff_vol'+label
+)
+
