5db008d1c0e59f27ede22ec074c60a4828d8e3ef
raphaeltimbo <raphaelts@gmail.com>
2018-04-11 12:59:06 -0300

    Add function to convert csv to yaml.

--- prf/load.py
+++ prf/load.py
@@ -1,10 +1,14 @@
+import os
+import csv
+import numpy as np
 import yaml
+from scipy.interpolate import UnivariateSpline
 from prf.state import State
 from prf.point import Point, Curve
 from prf.impeller import Impeller
 
 
-__all__ = ['load']
+__all__ = ['load', 'convert_csv_to_yaml']
 
 
 def load(file):
@@ -46,3 +50,59 @@ def load(file):
     imp = Impeller(curve, b=b, D=D)
 
     return imp
+
+def _interpolated_curve_from_csv(file):
+    """Convert from csv file to interpolated curve.
+
+    Function to convert from csv generated with engauge digitizer to an
+    interpolated curve.
+    """
+    flow_values = []
+    parameter = []
+
+    with open(file) as csvfile:
+        data = csv.reader(csvfile)
+        next(data)
+        for row in data:
+            flow_values.append(float(row[0]))
+            parameter.append(float(row[0]))
+
+    parameter_interpolated_curve = UnivariateSpline(flow_values, parameter)
+    
+    return parameter_interpolated_curve, flow_values
+
+def convert_csv_to_yaml(dir_path='', number_of_points_to_yaml=6):
+    """Convert csv head and eff from csv to yaml.
+    
+    Parameters:
+    -----------
+    dir_path : str
+        Path to directory with head and eff csv files.
+
+    Returns:
+    --------
+    head_eff : yaml file
+
+    """
+    head_file = os.path.join(dir_path, 'head.csv')
+    eff_file = os.path.join(dir_path, 'eff.csv')
+
+    head_interpolated_curve, flow_values = _interpolated_curve_from_csv(head_file)
+    eff_interpolated_curve, _ = _interpolated_curve_from_csv(eff_file)
+
+    data = {}
+
+    data['flow']= np.linspace(min(flow_values), max(flow_values),
+            number_of_points_to_yaml)
+
+    data['head'] = head_interpolated_curve(data['flow'])
+    data['eff'] = eff_interpolated_curve(data['flow']) / 1e2
+
+    data = {k: [round(float(i), 5) for i in v] for k, v in data.items()}
+    print(data)
+
+    yaml_file = os.path.join(dir_path, 'input_head_eff.yml')
+
+    with open(yaml_file, 'w') as f:
+        yaml.dump(data, f, default_flow_style=False)
+        
